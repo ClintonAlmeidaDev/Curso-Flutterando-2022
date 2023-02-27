1120c3446cce54487c8508ef5cbb588928720fcf
ClintonAlmeidaJs <clinton.almeida10@gmail.com>
2023-02-27 09:00:56 -0300

    Aula 23
    Utilizando Stack para Background e sobreposição

--- curso_flutterando_2022/lib/login_page.dart
+++ curso_flutterando_2022/lib/login_page.dart
@@ -14,58 +14,75 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String senha = '';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset('assets/images/logo.png'),
-                  // child: Image.network(
-                  //     'https://upload.wikimedia.org/wikipedia/commons/5/53/Wikimedia-logo.png'),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'E-mail'),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (value) {
-                    senha = value;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Password'),
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                    onPressed: (() {
-                      if (email == 'clinton.almeida10@gmail.com' &&
-                          senha == '123') {
-                        print('Correto');
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      }
-                    }),
-                    child: Text('Entrar'))
-              ],
-            ),
+
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                child: Image.asset('assets/images/logo.png'),
+                // child: Image.network(
+                //     'https://upload.wikimedia.org/wikipedia/commons/5/53/Wikimedia-logo.png'),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'E-mail'),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                onChanged: (value) {
+                  senha = value;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Password'),
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                  onPressed: (() {
+                    if (email == 'clinton.almeida10@gmail.com' &&
+                        senha == '123') {
+                      print('Correto');
+                      Navigator.of(context).pushReplacementNamed('/home');
+                    }
+                  }),
+                  child: Text('Entrar'))
+            ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/imageWallpaper.jpg',
+              fit: BoxFit.cover,
+            )),
+        Container(
+          color: Colors.black.withOpacity(0.3),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
